b8975f713b20df12c351affa2f504989d607b28e
Susan Jarvis <susjar@ceh.ac.uk>
2021-04-23 15:08:04 +0100

    Run basic logistic regression on simulated data

--- demo_modeldistributions.R
+++ demo_modeldistributions.R
@@ -15,6 +15,7 @@
 #' ## 1. Simulate distributions (or read in simulated spp)
 library(raster)
 library(virtualspecies)
+library(dismo)
 
 set.seed(1000)
 
@@ -46,12 +47,28 @@ dups <- pa_data[duplicated(pa_data[1:2]),]
 if(nrow(dups) == 0) {next} else {
   pa_data <- pa_data[!duplicated(pa_data[1:2]),]
 }
-pa_sets[[i]] <- pa_data
+pa_sets[[i]] <- list(pa_data = pa_data, Presence = pa_data[pa_data$Observed == 1,1:2], pseudoAbsence = pa_data[pa_data$Observed == 0,1:2])
+names(pa_sets)[i] <- paste0("Sp",i)
 }
 
-#' Maybe need to consider pseudo-absence weighting...to come back to 
+#' Pseudoabsence weighting dependent on model, Thomas' code has this accounted for
+
+
+#' 3. Run DECIDE models 
 #' 
-#' 
+#' Initially we can run just the logistic regression models as a test
+
+#source Edited Rob Functions
+devtools::source_url("https://raw.githubusercontent.com/TMondain/DECIDE_WP1/main/scripts/functions/Edited_Rob_Functions.R")
+
+#run model for first species
+lr_mod <- fsdm(species = "Sp1", model = "lr",
+     climDat = subset(hbv_y, subset = virt_comm1[[1]]$details$variables), spData = pa_sets, knots_gam = -1,
+     k = 4, 
+     write =  TRUE, outPath = "lr_outs/")
+
+lr_mod
+
 
 
 
