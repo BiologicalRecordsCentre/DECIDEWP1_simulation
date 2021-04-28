2873003c6abc93a484c83d2f87b8950f196c7998
Mondain - Monval <thoval@ceh.ac.uk>
2021-04-28 14:33:12 +0100

    commiting changes to function to run using slurm_apply() and the folder it creates - in future we don't want to commit the folder

--- slurm_run_sim_sdm.R
+++ slurm_run_sim_sdm.R
@@ -1,7 +1,7 @@
 #' # Run all 10 simulated species on LOTUS
 #' 
 slurm_run_sim_sdm <- function(index){
-#' 
+  #' 
   #' ## 1. Simulate distributions (or read in simulated spp)
   library(raster)
   library(virtualspecies)
@@ -64,79 +64,79 @@ slurm_run_sim_sdm <- function(index){
   #species index
   sp_list <- names(pa_sets)
   species <- sp_list[index]
-    
+  
   #subset envdata for species of interest  
   env_data <- subset(hbv_y, subset = virt_comm1[[index]]$details$variables)
   
   #set parameters
   model <- "lr"
   
-    
+  
   #run model for first species
   sdm <- fsdm(species = species, model = model,
-                climDat = env_data, spData = pa_sets, knots_gam = -1,
-                k = k, 
-                write =  TRUE, outPath = "lr_outs/")
-    
+              climDat = env_data, spData = pa_sets, knots_gam = -1,
+              k = k, 
+              write =  TRUE, outPath = "lr_outs/")
+  
   #predictions
-    
+  
   preds1 <- get_predictions(sdm, model, env_data)
-    
+  
   ## save files ##
   species_name <- gsub(pattern = ' ', replacement = '_', species) # get species name without space
-    
+  
   outPath <- paste0(getwd(), "/Outputs/")
-    
+  
   # save prediction raster
   writeRaster(x = preds1$mean_predictions, 
-                filename = paste0(outPath, model, "_SDMs_", species_name, "_meanpred.grd"),
-                format = 'raster', overwrite = T)
-    
+              filename = paste0(outPath, model, "_SDMs_", species_name, "_meanpred.grd"),
+              format = 'raster', overwrite = T)
+  
   # save quantile max min
   writeRaster(x = preds1$quant_minmax, 
-                filename = paste0(outPath, model, "_SDMs_", species_name, "_quantilemaxmin.grd"),
-                format = 'raster', overwrite = T)
-    
+              filename = paste0(outPath, model, "_SDMs_", species_name, "_quantilemaxmin.grd"),
+              format = 'raster', overwrite = T)
+  
   # save quantile range raster
   writeRaster(x = preds1$quant_range, 
-                filename = paste0(outPath, model, "_SDMs_", species_name, "_quantilerange.grd"),
-                format = 'raster', overwrite = T)
-    
+              filename = paste0(outPath, model, "_SDMs_", species_name, "_quantilerange.grd"),
+              format = 'raster', overwrite = T)
+  
   # write AUC to file for easy-access
   write.csv(x = data.frame(raw_AUC = sdm$AUC,
-                             meanAUC = sdm$meanAUC),
-              file = paste0(outPath, model, "_SDMs_", species_name, "_AUC_values.csv"))
-    
+                           meanAUC = sdm$meanAUC),
+            file = paste0(outPath, model, "_SDMs_", species_name, "_AUC_values.csv"))
+  
   # write data to file too
   write.csv(x = sdm$Data,
-              file = paste0(outPath, model, "_SDMs_", species_name, "_Data.csv"))
-    
+            file = paste0(outPath, model, "_SDMs_", species_name, "_Data.csv"))
+  
   # save subset model output
   # remove data from model output
   sdm$Data <- NULL
-    
+  
   # output of model to store
   model_output <- list(species = species_name,
-                         model = model,
-                         sdm_output = sdm,
-                         number_validations = k)
-    
+                       model = model,
+                       sdm_output = sdm,
+                       number_validations = k)
+  
   save(model_output, file = paste0(outPath, model, "_SDMs_", species_name, 
-                                     ".rdata"))
-    
-    
+                                   ".rdata"))
+  
+  
   #' Calculate very simple DECIDE score - prediction * quantile range
-    
+  
   DECIDE_score <- preds1$mean_predictions*preds1$quant_range
-   
+  
   writeRaster(x = DECIDE_score, 
-                  filename = paste0(outPath, model, "_SDMs_", species_name, "_DECIDEscore.grd"),
-                  format = 'raster', overwrite = T)
-      
+              filename = paste0(outPath, model, "_SDMs_", species_name, "_DECIDEscore.grd"),
+              format = 'raster', overwrite = T)
+  
   #' Plot maps
   #' 
   png(paste0("Plots/", species_name,".png"), height = 200, width = 200, res = 300, units = "mm", pointsize = 14)
-      
+  
   par(mfrow=c(3,2))
   par(mar = c(2,2,2,2))
   plot(virt_comm1[[index]], main = "Environmental suitability")
@@ -146,7 +146,26 @@ slurm_run_sim_sdm <- function(index){
   plot(preds1$mean_predictions, main = "Predicted prob. occ")
   plot(preds1$quant_range, main = "Quantile range of predictions")
   plot(DECIDE_score, main = "DECIDE score")
-      
-  dev.off()
-  }
   
+  dev.off()
+}
+
+## index file
+pars <- data.frame(index = seq(1:10)) # number of species
+
+library(rslurm)
+
+#### slurm apply call
+sdm_slurm <- slurm_apply(slurm_run_sim_sdm,
+                         params = pars,
+                         jobname = 'sdm_simulated_species',
+                         nodes = length(pars$index),
+                         cpus_per_node = 1,
+                         slurm_options = list(partition = 'short-serial',
+                                              time = '23:59:59',
+                                              mem = 20000), ## good to keep the error files in the right directory but don't know where you want to keep them on lotus
+                                              # error = paste0('/gws/nopw/j04/ceh_generic/thoval/DECIDE/SDMs/outputs/', taxa, '/log_files/SDM_Bootstrap_', model,'/SDM_Bootstrap_', model,'-%j-%a.out')),
+                           rscript_path = '',
+                         submit = F)
+
+
