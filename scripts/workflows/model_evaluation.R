225823f9c68c0a372326ee4646abb346b8f32d8a
Susan Jarvis <susjar@ceh.ac.uk>
2021-06-28 11:38:31 +0100

    Added basic plots in document

--- scripts/workflows/model_evaluation.R
+++ scripts/workflows/model_evaluation.R
@@ -18,25 +18,44 @@ for (i in 1:length(species_list)){
   
 }
 
+
+png("Community 1 results by method.png", height = 800, width = 400)
+par(mfrow=c(3,1))
 #boxplots by method
-boxplot(eval_out$mse_diff ~ eval_out$method)
-boxplot(eval_out$corr_diff ~ eval_out$method)
-boxplot(eval_out$auc_diff ~ eval_out$method)
+boxplot(eval_out$mse_diff ~ eval_out$method, xlab = "Method", ylab = "MSE difference from initial model")
+boxplot(eval_out$corr_diff ~ eval_out$method, xlab = "Method", ylab = "Correlation difference from initial model")
+boxplot(eval_out$auc_diff ~ eval_out$method, xlab = "Method", ylab = "AUC difference from initial model")
 
 
 #relationships with prevalence and initial model AUC
-par(mar = c(5,4,4,7))
-plot(eval_out$mse_diff ~ eval_out$prevalence, pch = 20, col = as.numeric(factor(eval_out$method)))
+
+png("Community 1 results against prevalence.png", height = 1600, width = 1200, res = 200, pointsize = 16)
+par(mfrow=c(3,1))
+par(mar = c(4,4,1,7))
+plot(eval_out$mse_diff ~ eval_out$prevalence, pch = 20, col = as.numeric(factor(eval_out$method)), xlab = "Prevalence", ylab = "MSE difference from initial model")
 par(xpd = TRUE)
 legend(x = 1.1, y = 0.1,legend = unique(eval_out$method), pch = 20, col = as.numeric(factor(eval_out$method)))
-plot(eval_out$corr_diff ~ eval_out$prevalence, pch = 20, col = as.numeric(factor(eval_out$method)))
+plot(eval_out$corr_diff ~ eval_out$prevalence, pch = 20, col = as.numeric(factor(eval_out$method)), xlab = "Prevalence", ylab = "Correlation difference from initial model")
 legend(x = 1.1, y = 0.1,legend = unique(eval_out$method), pch = 20, col = as.numeric(factor(eval_out$method)))
-plot(eval_out$auc_diff ~ eval_out$prevalence, pch = 20, col = as.numeric(factor(eval_out$method)))
+plot(eval_out$auc_diff ~ eval_out$prevalence, pch = 20, col = as.numeric(factor(eval_out$method)), xlab = "Prevalence", ylab = "AUC difference from initial model")
 legend(x = 1.1, y = 0.1,legend = unique(eval_out$method), pch = 20, col = as.numeric(factor(eval_out$method)))
+dev.off()
 
-plot(eval_out$mse_diff ~ eval_out$auc_initial)
-plot(eval_out$corr_diff ~ eval_out$auc_initial)
-plot(eval_out$auc_diff ~ eval_out$auc_initial)
+
+png("Community 1 results against AUC.png", height = 1600, width = 1200, res = 200, pointsize = 16)
+par(mfrow=c(3,1))
+par(mar = c(4,4,1,7))
+plot(eval_out$mse_diff ~ eval_out$auc_initial, pch = 20, col = as.numeric(factor(eval_out$method)), xlab = "AUC of initial model", ylab = "MSE difference from initial model")
+par(xpd = TRUE)
+legend(x = 1.1, y = 0.1,legend = unique(eval_out$method), pch = 20, col = as.numeric(factor(eval_out$method)))
+plot(eval_out$corr_diff ~ eval_out$auc_initial, pch = 20, col = as.numeric(factor(eval_out$method)), xlab = "AUC of initial model", ylab = "Correlation difference from initial model")
+legend(x = 1.1, y = 0.1,legend = unique(eval_out$method), pch = 20, col = as.numeric(factor(eval_out$method)))
+plot(eval_out$auc_diff ~ eval_out$auc_initial, pch = 20, col = as.numeric(factor(eval_out$method)), xlab = "AUC of initial model", ylab = "AUC difference from initial model")
+legend(x = 1.1, y = 0.1,legend = unique(eval_out$method), pch = 20, col = as.numeric(factor(eval_out$method)))
+dev.off()
+
+
+
 
 #average across species
 
