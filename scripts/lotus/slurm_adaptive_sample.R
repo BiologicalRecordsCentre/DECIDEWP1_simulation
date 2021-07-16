33f1c218f3d74c62f9c153837907eed7f1ee5ad7
Mondain - Monval <thoval@ceh.ac.uk>
2021-07-16 13:41:45 +0100

    Added code to adaptive_sample to base adaptive sampling on uncertainty*1/number of records. So areas with large numbers of records are downweighted. This is similar to the DECIDE score in the app currently but not identical. Identical would be time since last record, which would not be possible without using raw data.
    This has been briefly tested on Lotus but not using batch submission. Requires tidyverse.

--- scripts/lotus/slurm_adaptive_sample.R
+++ scripts/lotus/slurm_adaptive_sample.R
@@ -10,19 +10,19 @@ slurm_adaptive_sample <- function(community_file, sdm_path, effort, background, 
   
   #import env_data if specified
   if(!is.null(env_data)){
-  env <- raster::stack(env_data)
-  
-  #crop to extent if specified
-  if(!is.null(extent)){
-    e <- as(extent, "SpatialPolygons")
-    sp::proj4string(e) <- "+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0"
+    env <- raster::stack(env_data)
     
-    e.geo <- sp::spTransform(e, CRS("+proj=tmerc +lat_0=49 +lon_0=-2 +k=0.9996012717 +x_0=400000 +y_0=-100000 +ellps=airy +units=m +no_defs"))
+    #crop to extent if specified
+    if(!is.null(extent)){
+      e <- as(extent, "SpatialPolygons")
+      sp::proj4string(e) <- "+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0"
+      
+      e.geo <- sp::spTransform(e, CRS("+proj=tmerc +lat_0=49 +lon_0=-2 +k=0.9996012717 +x_0=400000 +y_0=-100000 +ellps=airy +units=m +no_defs"))
+      
+      env_extent <- raster::crop(env, e.geo)
+      
+    } else {env_extent <- env}
     
-    env_extent <- raster::crop(env, e.geo)
-    
-  } else {env_extent <- env}
-  
   }
   
   #set background if given, can indicate a layer in env_data or be a filepath to a raster
@@ -47,11 +47,11 @@ slurm_adaptive_sample <- function(community_file, sdm_path, effort, background, 
   community_preds <- list()
   
   for (j in 1:length(species_list)){
-  
+    
     species <- species_list[j]
     #get model outputs for this species
     models_to_read <- grep(paste0(species,"_"), models)
-  
+    
     #load outputs into list and extract prediction table
     model_outputs <- list()
     idx <- 1
@@ -75,7 +75,7 @@ slurm_adaptive_sample <- function(community_file, sdm_path, effort, background, 
     #store only the model average for now - could edit to store the individual model outputs if needed
     if(is.null(nrow(mod_average))){community_preds[[j]] <- NULL} else { community_preds[[j]] <- mod_average; names(community_preds)[j] <- species}
     
-  
+    
   }
   
   #average across all species in community (which can be modelled) to obtain a single prevalence, uncertainty and DECIDE score
@@ -171,6 +171,45 @@ slurm_adaptive_sample <- function(community_file, sdm_path, effort, background, 
     empty_cells <- eff_zero[eff_zero$butterfly_1km_effort == 0 & !is.na(eff_zero$butterfly_1km_effort),]#1 because we added 1 to the eff_layer to allow sampling in previously unvisited squares
     new_locs <- sample(1:nrow(empty_cells), size = n, replace = FALSE)#sample from empty cells with equal prob
     new_coords <- empty_cells[new_locs,1:2]
+  }
+  
+  if (method == "unc_plus_recs"){
+    
+    require(tidyverse)
+    
+    # get all the records that have been made in a community
+    # this is equivalent to eff_df but for the simulated data rather than the real butterfly data
+    obvs <- na.omit(do.call(rbind, lapply(community, function(x) x$observations)))
+    
+    # get the number of records in each recorded grid cell
+    num_recs <- obvs %>% mutate(x=lon,y=lat,lon=NULL,lat=NULL) %>% 
+      group_by(x, y) %>% tally 
+    
+    #combine effort and score dataframes
+    comb_df <- merge(community_scores, num_recs, by = c("x", "y"), all.x = TRUE)
+    comb_df$n[is.na(comb_df$n)] <- 0 # define NAs as 0; NAs are where there are no records
+    comb_df$n <- comb_df$n+1 # +1 to all so that when dividing by areas with no records, just get the unaltered uncertainty score
+    
+    # weighting (current score in DECIDE) is done by unc * 1 over number of records in a cell
+    # well actually, it's 1/time since last record - but that's not possible in our framework unless we use the raw 
+    # data which might cause problems...
+    comb_df$unc_recs <- comb_df$sd*(1/comb_df$n)
+    
+    if(is.null(uptake)){
+      cell_weights <- comb_df$unc_recs/sum(comb_df$unc_recs, na.rm=TRUE)
+    } else if(!is.null(uptake)) {
+      # combine with the specified effort layer dataset above so can see what happens
+      # if people carry on with business as usual instead of doing the sampling
+      comb_df_eff <- merge(eff_df, comb_df, by = c("x", "y"), all.x = TRUE)
+      comb_df_eff$comb_weight <- (comb_df_eff$layer*(1-uptake))+(comb_df_eff$unc_recs*uptake) # change the influence of adaptive sampling
+      cell_weights <- comb_df_eff$comb_weight/sum(comb_df_eff$comb_weight, na.rm=TRUE)
+    }
+    
+    #assign NA values the average weight
+    cell_weights[is.na(cell_weights)] <- mean(cell_weights, na.rm= TRUE)
+    #sample new locations according to cell weights
+    new_locs <- sample(1:nrow(community_scores), size = n, replace = FALSE, prob = cell_weights)
+    new_coords <- community_scores[new_locs, 1:2]
   }
   
   
